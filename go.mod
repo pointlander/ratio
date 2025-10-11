b37daf4f5d24877fed1b24c2a3afd980f07be8ca
Andrew Snodgrass <pointlander@gmail.com>
2025-10-11 18:23:32 -0500

    Added autoencoding

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/pointlander/ratio
 
 go 1.25.0
+
+require (
+	github.com/golang/protobuf v1.4.2 // indirect
+	github.com/pointlander/gradient v0.0.0-20250814141955-1993bf0b47d3 // indirect
+	github.com/ziutek/blas v0.0.0-20190227122918-da4ca23e90bb // indirect
+	google.golang.org/protobuf v1.24.0 // indirect
+)
--- main.go
+++ main.go
@@ -9,8 +9,32 @@ import (
 	"bytes"
 	"embed"
 	"encoding/csv"
+	"fmt"
 	"io"
+	"math"
+	"math/rand"
 	"strconv"
+	"strings"
+
+	"github.com/pointlander/gradient/tf64"
+)
+
+const (
+	// B1 exponential decay of the rate for the first moment estimates
+	B1 = 0.8
+	// B2 exponential decay rate for the second-moment estimates
+	B2 = 0.89
+	// Eta is the learning rate
+	Eta = 1.0e-1
+)
+
+const (
+	// StateM is the state for the mean
+	StateM = iota
+	// StateV is the state for the variance
+	StateV
+	// StateTotal is the total number of states
+	StateTotal
 )
 
 //go:embed iris.zip
@@ -89,5 +113,94 @@ func Load() []Fisher {
 }
 
 func main() {
-	Load()
+	rng := rand.New(rand.NewSource(1))
+	iris := Load()
+	others := tf64.NewSet()
+	others.Add("x", 4, len(iris))
+	x := others.ByName["x"]
+	for i := range iris {
+		for _, value := range iris[i].Measures {
+			x.X = append(x.X, value)
+		}
+	}
+
+	set := tf64.NewSet()
+	set.Add("y", 4, len(iris))
+	for i := range set.Weights {
+		w := set.Weights[i]
+		if strings.HasPrefix(w.N, "b") {
+			w.X = w.X[:cap(w.X)]
+			w.States = make([][]float64, StateTotal)
+			for ii := range w.States {
+				w.States[ii] = make([]float64, len(w.X))
+			}
+			continue
+		}
+		factor := math.Sqrt(2.0 / float64(w.S[0]))
+		for range cap(w.X) {
+			w.X = append(w.X, rng.NormFloat64()*factor)
+		}
+		w.States = make([][]float64, StateTotal)
+		for ii := range w.States {
+			w.States[ii] = make([]float64, len(w.X))
+		}
+	}
+
+	for iteration := range 128 {
+		pow := func(x float64) float64 {
+			y := math.Pow(x, float64(iteration+1))
+			if math.IsNaN(y) || math.IsInf(y, 0) {
+				return 0
+			}
+			return y
+		}
+		sum := tf64.Add(others.Get("x"), set.Get("y"))
+		l1 := tf64.T(tf64.Mul(tf64.Mul(sum, sum), tf64.T(sum)))
+		loss := tf64.Avg(tf64.Quadratic(l1, set.Get("y")))
+
+		l := 0.0
+		set.Zero()
+		others.Zero()
+		l = tf64.Gradient(loss).X[0]
+		if math.IsNaN(float64(l)) || math.IsInf(float64(l), 0) {
+			fmt.Println(iteration, l)
+			return
+		}
+		fmt.Println(l)
+
+		norm := 0.0
+		for _, p := range set.Weights {
+			for _, d := range p.D {
+				norm += d * d
+			}
+		}
+		norm = math.Sqrt(norm)
+		b1, b2 := pow(B1), pow(B2)
+		scaling := 1.0
+		if norm > 1 {
+			scaling = 1 / norm
+		}
+		for _, w := range set.Weights {
+			for ii, d := range w.D {
+				g := d * scaling
+				m := B1*w.States[StateM][ii] + (1-B1)*g
+				v := B2*w.States[StateV][ii] + (1-B2)*g*g
+				w.States[StateM][ii] = m
+				w.States[StateV][ii] = v
+				mhat := m / (1 - b1)
+				vhat := v / (1 - b2)
+				if vhat < 0 {
+					vhat = 0
+				}
+				w.X[ii] -= Eta * mhat / (math.Sqrt(vhat) + 1e-8)
+			}
+		}
+	}
+	y := set.ByName["y"]
+	for i := range len(iris) {
+		for ii := range 4 {
+			fmt.Printf("%f ", y.X[i*4+ii])
+		}
+		fmt.Println()
+	}
 }
