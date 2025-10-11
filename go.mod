be343c8c461c2b0077d8fa042588a4cb5d5cb045
Andrew Snodgrass <pointlander@gmail.com>
2025-10-11 13:01:10 -0500

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/pointlander/ratio
+
+go 1.25.0
---
+++ main.go
@@ -0,0 +1,93 @@
+// Copyright 2025 The Ratio Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"archive/zip"
+	"bytes"
+	"embed"
+	"encoding/csv"
+	"io"
+	"strconv"
+)
+
+//go:embed iris.zip
+var Iris embed.FS
+
+// Fisher is the fisher iris data
+type Fisher struct {
+	Measures []float64
+	Label    string
+	Cluster  int
+	Index    int
+}
+
+// Labels maps iris labels to ints
+var Labels = map[string]int{
+	"Iris-setosa":     0,
+	"Iris-versicolor": 1,
+	"Iris-virginica":  2,
+}
+
+// Inverse is the labels inverse map
+var Inverse = [3]string{
+	"Iris-setosa",
+	"Iris-versicolor",
+	"Iris-virginica",
+}
+
+// Load loads the iris data set
+func Load() []Fisher {
+	file, err := Iris.Open("iris.zip")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	data, err := io.ReadAll(file)
+	if err != nil {
+		panic(err)
+	}
+
+	fisher := make([]Fisher, 0, 8)
+	reader, err := zip.NewReader(bytes.NewReader(data), int64(len(data)))
+	if err != nil {
+		panic(err)
+	}
+	for _, f := range reader.File {
+		if f.Name == "iris.data" {
+			iris, err := f.Open()
+			if err != nil {
+				panic(err)
+			}
+			reader := csv.NewReader(iris)
+			data, err := reader.ReadAll()
+			if err != nil {
+				panic(err)
+			}
+			for i, item := range data {
+				record := Fisher{
+					Measures: make([]float64, 4),
+					Label:    item[4],
+					Index:    i,
+				}
+				for ii := range item[:4] {
+					f, err := strconv.ParseFloat(item[ii], 64)
+					if err != nil {
+						panic(err)
+					}
+					record.Measures[ii] = f
+				}
+				fisher = append(fisher, record)
+			}
+			iris.Close()
+		}
+	}
+	return fisher
+}
+
+func main() {
+	Load()
+}
